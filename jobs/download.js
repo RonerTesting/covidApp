aa498f70a1f6f0b65f6d9f048c2b5e4919f7bff3
Roner <roner.ortega@gmail.com>
2022-04-07 01:35:53 -0500

    Modelos & Servicios

---
+++ jobs/download.js
@@ -0,0 +1,15 @@
+#!/usr/bin/env node
+
+var child_process = require('child_process');
+
+async function runCmd(cmd)
+{
+  var resp = await child_process.exec(cmd);
+  var result = resp.toString('UTF8');
+  return result;
+}
+
+var cmd = `curl -s "https://services.arcgis.com/BQTQBNBsmxjF8vus/ArcGIS/rest/services/Colombia_COVID19V/FeatureServer/1/query?where=1%3D1&objectIds=&time=&geometry=&geometryType=esriGeometryEnvelope&inSR=&spatialRel=esriSpatialRelIntersects&resultType=none&distance=0.0&units=esriSRUnit_Meter&returnGeodetic=false&outFields=DPTO%2CNOMBRE_DPT%2CCoronavirus%2CCasos_Confirmados&returnGeometry=false&returnCentroid=false&featureEncoding=esriDefault&multipatchOption=xyFootprint&maxAllowableOffset=&geometryPrecision=&outSR=&defaultSR=&datumTransformation=&applyVCSProjection=false&returnIdsOnly=false&returnUniqueIdsOnly=false&returnCountOnly=false&returnExtentOnly=false&returnQueryGeometry=false&returnDistinctValues=false&cacheHint=false&orderByFields=&groupByFieldsForStatistics=&outStatistics=&having=&resultOffset=&resultRecordCount=&returnZ=false&returnM=false&returnExceededLimitFeatures=true&quantizationParameters=&sqlFormat=none&f=pjson&token=" | jq -r '.features' > dataDay.json`;  
+var result = runCmd(cmd);
+
+console.log(result);
--- server.js
+++ server.js
@@ -1,6 +1,18 @@
-const express = require('express');
+const express = require("express");
+const bodyParser = require("body-parser");
+const { ColombiaCovid } = require('./service/index')
 const app = express();
 
-app.get('/',(req,res)=>{
-    res.send('inicio...');
-})
+let covidData = new ColombiaCovid();
+covidData.getData();
+
+// console.log(data);
+const port = 3000;
+app.use(bodyParser.json());
+app.get("/", (req, res) => {
+  res.send("xd...");
+});
+
+app.listen(port, () => {
+  console.log("Mi port " + port);
+});
